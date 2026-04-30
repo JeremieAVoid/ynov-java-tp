21eb90ec329621a8e0c225fbaaa208db07787ee6
NardenLaSouris <neveu.jules58@gmail.com>
2026-04-30 08:39:52 +0200

    [UPDATE](livre.java) attributs livre

--- livre.java
+++ livre.java
@@ -3,4 +3,11 @@ public class livre {
     private String auteur;
     private int identifiant;
     private boolean disponible;
+
+    public Livre(String titre, String auteur, int identifiant) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.identifiant = identifiant;
+        this.disponible = true;
+    }
 }
